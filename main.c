c50a05d504557cb3994d8ed172e702abd514b8ce
nikomartn <nicomartinblog@gmail.com>
2017-04-16 03:01:36 +0200

    Construida función main

--- main.c
+++ main.c
@@ -8,12 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tipos.h"
+#include "funciones.h"
 
 INT main (void)
 {
 	/*Se recomienda leer el archivo leeme.txt donde se define el programa y sus diferentes archivos de código fuente*/
 
+	INT *polinomio = NULL;
+	INT grado = 0, x = 0;
+	CHAR salida = '0';
 
+	printf("Escriba el grado del polinomio:\t");
+	scanf("%d",&grado);
+
+	polinomio = lee_polinomio(grado);
+
+	printf("Introduzca el valor de una X\n");
+	scanf("%d",&x);
+
+	puts("");
+
+	printf("El resultado es: %d",evalua_polinomio(polinomio,grado,x));
 
 	return 0;
 }
